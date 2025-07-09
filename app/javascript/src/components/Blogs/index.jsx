83ecfb267b90249320979e17d89f491b87aaabeb
Prabodh Kumar Panda <connect@prabodh.co.in>
2025-07-09 17:24:50 +0530

    Added jsconfig

--- app/javascript/src/components/Blogs/index.jsx
+++ app/javascript/src/components/Blogs/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
-import { Button } from "@bigbinary/neetoui";
+import { Book } from "neetoicons";
+import { Button } from "neetoui";
 
 const Blogs = () => {
   const [count, setCount] = useState(0);
@@ -8,6 +9,7 @@ const Blogs = () => {
   return (
     <div>
       <p>{count}</p>
+      <Book />
       <Button>Hello</Button>
       <button onClick={() => setCount(prev => prev + 1)}>Increment</button>
     </div>
--- config/build/config.js
+++ config/build/config.js
@@ -15,6 +15,17 @@ const alias = {
   apis: absolutePath("src/apis"),
   common: absolutePath("src/common"),
   components: absolutePath("src/components"),
+  constants: absolutePath("src/constants"),
+  hooks: absolutePath("src/hooks"),
+  reducers: absolutePath("src/reducers"),
+  routes: absolutePath("src/routes"),
+  stores: absolutePath("src/stores"),
+  translations: absolutePath("src/translations"),
+  utils: absolutePath("src/utils"),
+  src: absolutePath("src"),
+  neetoui: "@bigbinary/neetoui",
+  neetocist: "@bigbinary/neeto-cist",
+  neetoicons: "@bigbinary/neeto-icons",
 };
 
 export { alias };
