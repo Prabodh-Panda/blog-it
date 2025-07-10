0436b28c0de7e18b63e0caee72c80cf0a8b48344
Prabodh Kumar Panda <connect@prabodh.co.in>
2025-07-10 11:27:17 +0530

    Fixed hover effect of SidebarItem

--- app/javascript/src/components/commons/Sidebar/Item.jsx
+++ app/javascript/src/components/commons/Sidebar/Item.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 
 const Item = ({ to, icon }) => (
   <NavLink
-    activeClassName="bg-black text-white hover:bg-black"
+    activeClassName="bg-black text-white hover:!bg-black"
     className="block rounded-md  p-2 hover:bg-gray-200"
     {...{ to }}
   >
