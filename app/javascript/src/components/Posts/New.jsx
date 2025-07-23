3ae8e513fa446f2526015fac275d9458d3566a1b
Prabodh Kumar Panda <connect@prabodh.co.in>
2025-07-23 18:42:07 +0530

    Added the ability to save posts as draft from new blog post page

--- app/javascript/src/components/Posts/New.jsx
+++ app/javascript/src/components/Posts/New.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 import { Header, PageLoader, Sidebar } from "components/commons";
 import {
@@ -6,7 +6,7 @@ import {
   useFetchCategories,
 } from "hooks/reactQuery/useCategories";
 import { useCreatePost } from "hooks/reactQuery/usePosts";
-import { Button } from "neetoui";
+import { ActionDropdown, Button } from "neetoui";
 import { Form, Input, Textarea, Select } from "neetoui/formik";
 import { useTranslation } from "react-i18next";
 import { useHistory } from "react-router-dom";
@@ -20,8 +20,17 @@ import {
 } from "./constants";
 import { getCategoryOptions } from "./utils";
 
+const {
+  Menu,
+  MenuItem: { Button: MenuItemButton },
+} = ActionDropdown;
+
 const New = () => {
+  const [status, setStatus] = useState("published");
+
   const { t } = useTranslation();
+
+  const formikRef = useRef();
 
   const { mutate: createPost, isLoading: isCreatePostLoading } =
     useCreatePost();
@@ -37,16 +46,19 @@ const New = () => {
   const handleSubmit = async postData => {
     const payload = {
       ...postData,
+      status,
       category_ids: postData.categories.map(category => category.value),
-      user_id: 1,
-      organization_id: 1,
     };
 
     createPost(payload, {
       onSuccess: () => {
-        history.push(routes.posts.index);
+        history.push(routes.myPosts.index);
       },
     });
+  };
+
+  const handleSubmitButtonClick = () => {
+    if (formikRef.current) formikRef.current.submitForm();
   };
 
   const handleCreateCategory = name => {
@@ -61,13 +73,40 @@ const New = () => {
     <div className="flex h-screen w-screen">
       <Sidebar />
       <div className="flex flex-1 flex-col">
-        <Header title={t("titles.newBlogPost")} />
+        <Header
+          title={t("titles.newBlogPost")}
+          actionBlock={
+            <div className="ml-auto mt-auto space-x-2">
+              <Button
+                disabled={isCreatePostLoading}
+                style="secondary"
+                to={routes.posts.index}
+              >
+                {t("labels.cancel")}
+              </Button>
+              <ActionDropdown
+                label={status === "published" ? "Publish" : "Save as draft"}
+                onClick={handleSubmitButtonClick}
+              >
+                <Menu>
+                  <MenuItemButton onClick={() => setStatus("published")}>
+                    Publish
+                  </MenuItemButton>
+                  <MenuItemButton onClick={() => setStatus("draft")}>
+                    Save as draft
+                  </MenuItemButton>
+                </Menu>
+              </ActionDropdown>
+            </div>
+          }
+        />
         <div className="w-full flex-1 px-16 pb-10">
           <Form
             className="flex h-full flex-col justify-start rounded-lg border p-10"
             formikProps={{
               initialValues: NEW_POST_INITIAL_VALUES,
               validationSchema: NEW_POST_VALIDATION_SCHEMA,
+              innerRef: formikRef,
               onSubmit: handleSubmit,
             }}
           >
@@ -95,22 +134,6 @@ const New = () => {
                 name="description"
                 placeholder={t("placeholders.description")}
               />
-            </div>
-            <div className="ml-auto mt-auto space-x-2">
-              <Button
-                disabled={isCreatePostLoading}
-                style="secondary"
-                to={routes.posts.index}
-              >
-                {t("labels.cancel")}
-              </Button>
-              <Button
-                className="bg-black"
-                disabled={isCreatePostLoading}
-                type="submit"
-              >
-                {t("labels.submit")}
-              </Button>
             </div>
           </Form>
         </div>
