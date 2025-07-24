e2abccabfd85588c0eb54d3a9aee6415bcef743f
Prabodh Kumar Panda <connect@prabodh.co.in>
2025-07-24 23:48:54 +0530

    Added user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
+    authorize user
     user.save!
     render_notice(t("successfully_created", entity: "User"))
   end
