867d1ef0dfca363b71b8d48a0a1aca6a0e82bd0c
Prabodh Kumar Panda <connect@prabodh.co.in>
2025-07-25 17:21:23 +0530

    Used jbuilder for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,8 +5,7 @@ class CategoriesController < ApplicationController
   after_action :verify_policy_scoped, only: :index
 
   def index
-    categories = policy_scope(Category)
-    render status: :ok, json: { categories: }
+    @categories = policy_scope(Category)
   end
 
   def create
