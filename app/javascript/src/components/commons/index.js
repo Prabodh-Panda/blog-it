f311e9a0df4ea45f7231d69c0e0142e3ab0e2206
Prabodh Kumar Panda <connect@prabodh.co.in>
2025-07-25 11:05:17 +0530

    Added missing exports to commons/index.js

--- app/javascript/src/components/commons/index.js
+++ app/javascript/src/components/commons/index.js
@@ -1,6 +1,8 @@
 import Header from "./Header";
+import NotFound from "./NotFound";
 import PageLoader from "./PageLoader";
+import PageNotFound from "./PageNotFound";
 import PrivateRoute from "./PrivateRoute";
 import Sidebar from "./Sidebar";
 
-export { Header, Sidebar, PageLoader, PrivateRoute };
+export { Header, Sidebar, PageLoader, PrivateRoute, PageNotFound, NotFound };
