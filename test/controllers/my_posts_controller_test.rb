d31f9326b6da12b0051844cd031ea7f2fedcb05b
Prabodh Kumar Panda <connect@prabodh.co.in>
2025-07-29 15:16:52 +0530

    Added tests for my_posts_controller

--- test/controllers/my_posts_controller_test.rb
+++ test/controllers/my_posts_controller_test.rb
@@ -3,7 +3,34 @@
 require "test_helper"
 
 class MyPostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @organization = create(:organization)
+    @user = create(:user, organization: @organization)
+    @headers = headers(@user)
+  end
+
+  def test_index_should_return_only_users_posts
+    user_published_posts = create_list(:post, 3, user: @user, organization: @organization, status: "published")
+    user_draft_posts = create_list(:post, 3, user: @user, organization: @organization, status: "draft")
+
+    other_users = create_list(:user, 3, organization: @organization)
+    other_posts = []
+
+    5.times do |i|
+      user = other_users[i % other_users.size]
+      other_posts << create(:post, user: user, organization: @organization, status: "published")
+    end
+
+    get my_posts_path, headers: @headers
+    assert_response :success
+
+    returned_post_ids = response.parsed_body["posts"].pluck(:id)
+    expected_post_ids = (user_published_posts + user_draft_posts).pluck(:id)
+
+    assert_equal expected_post_ids.sort, returned_post_ids.sort
+
+    other_posts.each do |post|
+      refute_includes returned_post_ids, post.id
+    end
+  end
 end
