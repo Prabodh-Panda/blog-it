6a19c9f629d83402126f70ceb15dbc3d9fa8755a
Prabodh Kumar Panda <connect@prabodh.co.in>
2025-07-29 16:00:10 +0530

    Added more tests to organization_test

--- test/models/organization_test.rb
+++ test/models/organization_test.rb
@@ -3,9 +3,26 @@
 require "test_helper"
 
 class OrganizationTest < ActiveSupport::TestCase
+  def setup
+    @organization = create(:organization)
+  end
+
   def test_valid_factory
     organization = build(:organization)
-    assert organization.valid?, "Expected factory-built organization to be valid"
+    assert organization.valid?
+  end
+
+  def test_values_of_created_at_and_updated_at
+    organization = build(:organization)
+    assert_nil organization.created_at
+    assert_nil organization.updated_at
+
+    organization.save!
+    assert_not_nil organization.created_at
+    assert_equal organization.updated_at, organization.created_at
+
+    organization.update!(name: "This is a updated organization")
+    assert_not_equal organization.updated_at, organization.created_at
   end
 
   def test_is_invalid_without_a_name
@@ -21,5 +38,17 @@ class OrganizationTest < ActiveSupport::TestCase
 
     assert_not duplicate.valid?
     assert_includes duplicate.errors[:name], "has already been taken"
+  end
+
+  def test_organization_count_increases_on_saving
+    assert_difference ["Organization.count"], 1 do
+      create(:organization)
+    end
+  end
+
+  def test_organization_count_decreases_on_deleting
+    assert_difference ["Organization.count"], -1 do
+      @organization.destroy
+    end
   end
 end
