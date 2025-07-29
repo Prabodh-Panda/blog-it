170619928112df74a4d00015b8b7e58c7d62bcba
Prabodh Kumar Panda <connect@prabodh.co.in>
2025-07-29 10:08:19 +0530

    Added missing translation for getLastUpdatedAtDateTimeString

--- app/javascript/src/components/MyPosts/Table/utils.js
+++ app/javascript/src/components/MyPosts/Table/utils.js
@@ -1,9 +1,10 @@
 import dayjs from "dayjs";
+import { t } from "i18next";
 
 export const getLastUpdatedAtDateTimeString = timestamp =>
   timestamp
     ? dayjs(timestamp).format("DD MMM, YYYY, hh:mm A")
-    : "Has not been published";
+    : t("messages.notPublished");
 
 export const getCategoriesString = categories =>
   categories.map(category => category.name).join(", ");
